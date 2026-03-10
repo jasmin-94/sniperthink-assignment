20f3a2d544fee864cd3673d0ba5dd837d6cddfec
jasmin-94 <jasminepishike@gmail.com>
2026-03-10 19:58:56 +0530

    connect frontend to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -3,12 +3,14 @@ import axios from "axios";
 export const sendInterest = async (data) => {
   try {
     const response = await axios.post(
-      "http://localhost:5000/api/interest",
+      "https://sniperthink-assignment-tgbt.onrender.com/api/interest",
       data
     );
 
     return response.data;
+
   } catch (error) {
-    console.error(error);
+    console.error("API Error:", error);
+    return { error: true };
   }
 };
